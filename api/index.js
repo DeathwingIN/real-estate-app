424425aea01d3f90d870e35581153b075bfa5696
DeathwingIN <imeshnirmal1u@gmail.com>
2024-01-21 15:44:10 +0530

    Connected to MONGO

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MONGO");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running port 3000");
+  console.log("Server is running port 300");
 });
