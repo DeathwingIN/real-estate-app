3914d973fcd0d1a727cb7275d188b49e9b8619f5
Gnilu <gayani2000nilushi@gmail.com>
2024-03-06 23:15:42 -0800

    complete profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
+
 export default function Header() {
+  
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
