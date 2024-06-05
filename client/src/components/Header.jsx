3fd1aea17fb6ebad411d74203ee40f2f710a5249
disini000 <disinichathumika@gmail.com>
2024-06-05 20:26:22 +0530

    Update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,37 @@
-import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+//import { useEffect, useState } from 'react';//
 
 export default function Header() {
-  
-  return (
-    <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">PrimeNest</span>
-            <span className="text-slate-700">Estates</span>
+  const { currentUser } = useSelector((state) => state.user);
+  //const [searchTerm, setSearchTerm] = useState('');//
+  //const navigate = useNavigate();//
+  //const handleSubmit = (e) => {
+   // e.preventDefault();//
+   // const urlParams = new URLSearchParams(window.location.search);//
+   // urlParams.set('searchTerm', searchTerm);//
+    //const searchQuery = urlParams.toString();//
+    //navigate(`/search?${searchQuery}`);//
+ // };
+
+ // useEffect(() => {
+    //const urlParams = new URLSearchParams(location.search);
+    //const searchTermFromUrl = urlParams.get('searchTerm');
+    //if (searchTermFromUrl) {
+      //setSearchTerm(searchTermFromUrl);
+   // }
+  //}, [location.search]);
+ return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>PrimeNest</span>
+            <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
+        
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -21,23 +40,32 @@ export default function Header() {
           />
           <FaSearch className="text-slate-500" />
         </form>
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
               Home
             </li>
           </Link>
-          <Link to="/About">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
           </Link>
-          <Link to="/Sign-in">
-            <li className="  text-slate-700 hover:underline">Sign in </li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
           </Link>
-          
+
         </ul>
       </div>
     </header>
-  );
-}
+  )
+};
