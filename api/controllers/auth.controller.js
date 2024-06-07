90078ad277dac46b997c2e721dc9aa26157b2305
disini000 <disinichathumika@gmail.com>
2024-06-07 22:58:31 +0530

    Complete image upload function, update user functionality & add delete, signout function, & create listing Api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -115,4 +115,13 @@ export const google = async(red, res, next) =>{
     next (error)
     
   }
-}
+};
+
+ export const signOut = async(req, res, next) =>{
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
+    next(error);
+  }
+ };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -29,6 +30,8 @@ app.listen(3000, () => {
 // Create API Route
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 
 //Create Middleware
 app.use((err, req, res, next) => {
