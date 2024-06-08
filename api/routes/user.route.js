74d753a81db391dc93144079e9a6bfdd28d1062a
disini000 <disinichathumika@gmail.com>
2024-06-08 15:42:20 +0530

    create update listing api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,9 @@ import express from "express";
 import { deleteUser, getUserListings, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
+
 const router = express.Router();
+
 router.get("/test",test );
 router.post('/update/:id', verifyToken updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
